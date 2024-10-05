17c4fea565f0e5c1913f9deb352a2e1b5cc6c5fd
Alex Castillo <alex@lexdrel.com>
2024-10-05 00:59:32 +0000

    solution completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-String inTime = "02:00PM";
 
-Console.WriteLine(DateTime.Parse(inTime));
+String inTime1 = "12:00:00PM";
+String inTime2 = "12:00:00aM";
+String inTime3 = "07:05:45PM";
+String inTime4 = "07:05:45am";
+String inTime5 = "13:52:00PM";
+
+Console.WriteLine(Result.TimeConversion(inTime1));
+Console.WriteLine(Result.TimeConversion(inTime2));
+Console.WriteLine(Result.TimeConversion(inTime3));
+Console.WriteLine(Result.TimeConversion(inTime4));
+Console.WriteLine(Result.TimeConversion(inTime5));
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string TimeConversion(string s)
+    {
+        return TimeOnly.Parse(s).ToString("HH:mm:ss");
+    }
+
+}
